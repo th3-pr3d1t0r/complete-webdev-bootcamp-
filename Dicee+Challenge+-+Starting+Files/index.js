40f50a41fd6aacf5342f6b39817086807a4d0aa1
pr3d1t0r <ugochukwuanosike1@gmail.com>
2025-03-12 14:35:54 +0100

    project files for webdev bootcamp

---
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,6 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1
+var randomNumber2 = Math.floor(Math.random()*6) + 1
+
+document.querySelector("img img1").setAttribute("images/dice"+randomNumber1+".png");
+
+document.querySelector("img img2").setAttribute("images/dice"+randomNumber2+".png");
---
+++ script.js
@@ -0,0 +1,97 @@
+/*function months(age){
+    var currentMonths = age * 12 ;
+    var monthLeft = 1080 -currentMonths;
+    return monthLeft
+}
+
+function weeks(age){
+    var currentWeeks = age*52;
+    var weeksLeft = 4680 - currentWeeks;
+    return weeksLeft
+}
+
+function days(age){
+    var currentDays = age * 365;
+    var daysLeft = 32850 - currentDays
+    return daysLeft;
+}
+
+
+console.log("You have",days(23),"days,",weeks(23),"weeks, and",months(23),"months left.");
+
+       
+    if(year % 4 === 0){
+        
+        if(year % 100 === 0){
+
+            if(year % 400 === 0){
+                return "Leap year"
+            }
+            else{
+                return "Not Leap Year"
+            }
+            
+           
+            
+        }else{
+             return "Leap Year"
+          
+        }
+        
+    }
+    else{
+        return "Not leap year."
+    }
+
+
+
+
+var username = prompt("what is your name")
+var guestList = ["angel", "bruno","mars","marko","killua"];
+var varifyStatus = guestList.includes(username);
+if (varifyStatus){
+    alert("welcome to the party",username);
+
+}
+else{
+    alert("you were not invited get out");
+}*/
+/*
+function fibonacciGenerator (n) {
+    //Do NOT change any of the code above 👆
+                                                           
+         var x = [0,1];
+         n = n - x.length;
+        
+        //Write your code here:
+        for(var i = 0; i<n; i++){
+           
+            var b = x[i] + x[i+1];
+            
+            x.push(b);
+            
+        }
+        var a =[];
+        for(var i = 0; i<= n+1; i++){
+            a.push(x[i]);
+
+        }
+        return a ;
+       
+        
+        
+        
+        
+        
+        
+        
+        //Return an array of fibonacci numbers starting from 0.
+        
+    //Do NOT change any of the code below 👇
+    }
+
+    fibonacciGenerator(6);
+
+    */
+   alert("hello world")
+    
