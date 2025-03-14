37aa84cf77b200b3411e8b01658abd18b9cd8349
pr3d1t0r <ugochukwuanosike1@gmail.com>
2025-03-14 09:53:43 +0100

    added button functionallity

--- Drum+Kit+Starting+Files/Drum_Kit_Starting_Files/index.js
+++ Drum+Kit+Starting+Files/Drum_Kit_Starting_Files/index.js
@@ -1,4 +1,5 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+  
 
 for(var i = 0; i < numberOfDrumButtons ; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
@@ -41,8 +42,39 @@ for(var i = 0; i < numberOfDrumButtons ; i++){
     })
 }
 
-document.addEventListener("keydown",function(){
-    alert("key was pressed");
+document.addEventListener("keydown",function(e){
+    var keypressed =  e.key;
+    switch(keypressed){
+        case "w":
+            var audio = new Audio("/Drum+Kit+Starting+Files/Drum_Kit_Starting_Files/sounds/crash.mp3");
+            audio.play(); 
+            break;
+        case "a":
+            var audio = new Audio("/Drum+Kit+Starting+Files/Drum_Kit_Starting_Files/sounds/kick-bass.mp3");
+            audio.play(); 
+            break;
+        case "s":
+            var audio = new Audio("/Drum+Kit+Starting+Files/Drum_Kit_Starting_Files/sounds/snare.mp3");
+            audio.play(); 
+            break;
+        case "d":
+            var audio = new Audio("/Drum+Kit+Starting+Files/Drum_Kit_Starting_Files/sounds/tom-1.mp3");
+            audio.play(); 
+            break;
+        case "j":
+            var audio = new Audio("/Drum+Kit+Starting+Files/Drum_Kit_Starting_Files/sounds/tom-2.mp3");
+            audio.play(); 
+            break;
+        case "k":
+            var audio = new Audio("/Drum+Kit+Starting+Files/Drum_Kit_Starting_Files/sounds/tom-3.mp3");
+            audio.play(); 
+            break;
+        case "l":
+            var audio = new Audio("/Drum+Kit+Starting+Files/Drum_Kit_Starting_Files/sounds/tom-4.mp3");
+            audio.play(); 
+            break;
+        
+    }
 })
 
 //document.querySelector("button").addEventListener("click",handleClicks)
