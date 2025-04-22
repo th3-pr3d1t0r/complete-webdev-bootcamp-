237ef0288bac91cc192ef01441f3bc34de0a5c5a
pr3d1t0r <ugochukwuanosike1@gmail.com>
2025-04-22 10:02:45 +0100

    updated code from corse

--- 3.5 Secrets Project/index.js
+++ 3.5 Secrets Project/index.js
@@ -1,7 +1,53 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
-import express from "express"
-import bodyParser from "body-parser"
+import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
 
-const port = 3000
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+function validateInput(req,res,next){
+    var response = req.body;
+    if(response.password =="ILoveProgramming"){
+        res.sendFile(__dirname+"/public/secret.html");
+
+    }
+    else{
+        res.sendFile(__dirname+"/public/index.html");
+//you can also use redirect()
+    }
+    next()
+}
+
+
+const app = express();
+const port = 3000;
+
+
+
+app.get("/" , (req,res) =>{
+  res.sendFile(__dirname+"/public/index.html");
+});
+
+
+
+app.use(bodyParser.urlencoded({extends:true}));
+
+app.post("/check",(req,res) => {
+    var response = req.body;
+    if(response.password =="ILoveProgramming"){
+        res.sendFile(__dirname+"/public/secret.html");
+
+    }
+    else{
+        res.sendFile(__dirname+"/public/index.html");
+
+    }
+});
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
